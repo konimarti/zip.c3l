f91cdaf514f03b3c851928ce068a7f8b82c7af7a
Koni Marti <koni.marti@gmail.com>
2026-01-28 21:46:04 +0100

    archive: use cwd as base for path prefix stripping

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-set -x
+# set -x
 
 c3c test 2> /dev/null
 ./build/testrun
@@ -13,44 +13,45 @@ zipar="./build/zipar"
 zipex="./build/zipex"
 zipfile="/tmp/tmp.zip"
 
-#set -e
+cleanup()
+{
+	rm $zipfile 2> /dev/null
+	rm -r /tmp/resources
+}
 
 #
 # zip round-trip
 #
-
-# run test
+echo
+echo "************************************"
+echo "*** (1) zipar / zipex round-trip ***"
+echo "************************************"
 $zipar $zipfile ./resources/test_zip
 $zipinfo $zipfile
 $zipex $zipfile /tmp
-diff -sr resources/test_zip /tmp/test_zip
-
-# cleanup
-rm $zipfile
-rm -r /tmp/test_zip
+diff -sr resources/test_zip /tmp/resources/test_zip
+cleanup
 
 #
 # Validate zipex against zip -r6
 #
-
-# run test
+echo
+echo "**************************************"
+echo "*** (2) zip -r6 / zipex round-trip ***"
+echo "**************************************"
 zip -r6 $zipfile ./resources/test_zip
 $zipex $zipfile /tmp
 diff -sr resources/test_zip /tmp/resources/test_zip
-
-# cleanup
-rm $zipfile
-rm -r /tmp/resources/test_zip
+cleanup
 
 #
 # Validate zipar against unzip
 #
-
-# run test
+echo
+echo "************************************"
+echo "*** (3) zipar / unzip round-trip ***"
+echo "************************************"
 $zipar $zipfile ./resources/test_zip
 unzip $zipfile -d /tmp
-diff -sr resources/test_zip /tmp/test_zip
-
-# cleanup
-rm $zipfile
-rm -r /tmp/test_zip
+diff -sr resources/test_zip /tmp/resources/test_zip
+cleanup
